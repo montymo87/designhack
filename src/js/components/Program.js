f90ca301175f07a702b804bab1744592c099ecb2
arma <montymo87@gmail.com>
2022-07-21 22:41:47 +0300

    new improovments

--- src/js/components/Program.js
+++ src/js/components/Program.js
@@ -1,12 +1,27 @@
 import 'ScssComponents/_program.scss';
+import Masonry from 'masonry-layout';
 
 const Program = () => {
 	function showMore() {
 		const btn = document.querySelector('.ProgBtn');
 		const list = document.querySelector('.program__list');
+		let flag = false;
+		let msnry;
 
 		btn.addEventListener('click', () => {
 			list.classList.toggle('program__list--active_state');
+
+			if (flag) {
+				flag = false;
+				msnry.destroy();
+				list.scrollIntoView();
+			} else {
+				flag = true;
+				msnry = new Masonry('.program__list', {
+					itemSelector: '.program__item',
+					horizontalOrder: true,
+				});
+			}
 		});
 	}
 	showMore();
